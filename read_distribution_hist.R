7fb49af4e0908a215db3701b16501e780ab5d814
stl-23 <73641317+stl-23@users.noreply.github.com>
2021-03-04 16:52:01 +0800

    Update read_distribution_hist.R

--- read_distribution_hist.R
+++ read_distribution_hist.R
@@ -1,7 +1,9 @@
 args <- commandArgs(T)
 data <- read.table(args[1],header=F)
+prefix <- args[2]
+pdf(paste(prefix,'read_distribution_hist.pdf'))
+png(paste(prefix,'read_distribution_hist.png'))
 p <- hist(data$V1,breaks = 100,col='blue',main='Subreads Length Distribution',xlab='Read Length',ylab='Read Number')
-pdf('read_distribution_hist.pdf')
-png('read_distribution_hist.png')
+p
 dev.off()
 
